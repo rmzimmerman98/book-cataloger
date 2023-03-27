3c1ed851d25449732f817095067bc02cdd1e668a
Dominique Clark <melaydominique@gmail.com>
2023-03-27 16:15:39 -0400

    add component

--- front_end/book-cataloger/src/App.js
+++ front_end/book-cataloger/src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import {useState, useEffect} from 'react'
 import axios from 'axios'
+import Add from './components/Add'
 
 const App = () => {
-  //states
+
   const [books, setBooks] = useState([])
-  //api request
+ 
   const getBooks = () => {
     axios.get('http://localhost:3000/books').then((response) => {
       setBooks(response.data)
     })
   }
+  useEffect(() => getBooks(), [])
   return (
     <>
       <h1>Testing</h1>
+      <Add getBooks={getBooks}/>
     </>
   )
    

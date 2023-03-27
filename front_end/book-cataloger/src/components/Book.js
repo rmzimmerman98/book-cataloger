5d40b9037ea84d9d53c91c8f61457d516ff774cd
Dominique Clark <melaydominique@gmail.com>
2023-03-27 17:39:15 -0400

    changes

--- front_end/book-cataloger/src/components/Book.js
+++ front_end/book-cataloger/src/components/Book.js
@@ -2,11 +2,12 @@ const Book = (props) => {
     return (
         <div>
             <div key={props.book._id}>
-                <h3>{props.book.title}</h3>
-                <h4>{props.book.author}</h4>
-                <h4>{props.book.publisher}</h4>
-                <h6>{props.book.pages}</h6>
-                <p>{props.book.summary}</p>
+                <h2>Title: {props.book.title}</h2>
+                <h4>Author: {props.book.author}</h4>
+                <h4>Publisher: {props.book.publisher}</h4>
+                <h4>Pages: {props.book.pages}</h4>
+                <h4>Summary: {props.book.summary}</h4>
+                <h4>Finished Reading: {props.book.read ? <>true</> : <>false</>}</h4>
             </div>
         </div>
     )
